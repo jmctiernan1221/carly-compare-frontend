c16396fde37b545c9c34fb45f3be489a1fd1574a
jmctiernan1221 <jmctiernan1221@gmail.com>
2025-06-27 12:40:20 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -62,7 +62,8 @@ export default function LandingPage() {
 
         {/* White Card with background image on desktop only */}
         <div
-          className="relative bg-white/90 rounded-xl shadow-2xl p-6 md:p-20 pt-28 mt-12 md:mt-24 flex flex-col justify-center"
+          <div className="relative bg-white/90 rounded-xl shadow-2xl p-6 md:p-20 pt-28 mt-12 md:mt-24 md:-mt-12 flex flex-col justify-center"
+
  style={
   isDesktop
     ? {
