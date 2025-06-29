ef4c67baf8fb8a5c9cdbc15669fdba3d2e20be13
jmctiernan1221 <jmctiernan1221@gmail.com>
2025-06-29 12:19:43 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -130,7 +130,7 @@ className="relative min-h-screen overflow-x-hidden bg-cover bg-bottom bg-no-repe
                 value={formData.name}
                 onChange={handleChange}
                 required
-                className="w-full p-3 text-sm border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+className="w-full p-3 text-sm text-gray-900 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
               <input
                 type="email"
@@ -139,7 +139,7 @@ className="relative min-h-screen overflow-x-hidden bg-cover bg-bottom bg-no-repe
                 value={formData.email}
                 onChange={handleChange}
                 required
-                className="w-full p-3 text-sm border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+className="w-full p-3 text-sm text-gray-900 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
               <input
                 type="text"
@@ -147,7 +147,7 @@ className="relative min-h-screen overflow-x-hidden bg-cover bg-bottom bg-no-repe
                 placeholder="Car Make"
                 value={formData.make}
                 onChange={handleChange}
-                className="w-full p-3 text-sm border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+className="w-full p-3 text-sm text-gray-900 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
 
               {/* ✅ Checkbox */}
