e737c6e3bcfc243b4e097fe29731b1df22f00b8e
jmctiernan1221 <jmctiernan1221@gmail.com>
2025-07-07 09:31:13 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -63,7 +63,7 @@ className="relative min-h-screen overflow-x-hidden bg-cover bg-bottom bg-no-repe
     className={`${
       isDesktop
         ? 'absolute z-10 object-contain animate-fade-bounce-once top-0 -right-6'
-        : 'absolute z-10 object-contain top-0 right-[0.5rem] w-[180px] translate-y-[24px]'
+        : 'absolute z-10 object-contain top-0 right-1 w-[180px] translate-y-[16px]'
     }`}
     priority
   />
